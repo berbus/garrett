6fccbb9a5cf6b069d30a1e9a668c75157aa89e66
Alberto <berbus8@gmail.com>
2022-08-13 16:14:16 +0200

    Threat model titles should be unique

--- api/models/threat_model.py
+++ api/models/threat_model.py
@@ -9,7 +9,7 @@ from .review import Review
 
 class ThreatModel(models.Model):
     oid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=200, unique=True)
     creation_date = models.DateField(default=datetime.date.today)
     completion_date = models.DateField(default=None, null=True)
 
