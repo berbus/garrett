8b087463819fd40cccbf2476f21f4a2e54f4f3e5
Alberto <berbus8@gmail.com>
2022-08-17 21:55:00 +0200

    Return test case details in finding

--- api/serializers/finding_serializer.py
+++ api/serializers/finding_serializer.py
@@ -1,11 +1,14 @@
-
 from rest_framework import serializers
 
 import api.models as models
 
 
 class FindingSerializer(serializers.ModelSerializer):
+    test_case = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = models.Finding
         fields = '__all__'
+
+    def get_test_case(self, finding):
+        return {'oid': finding.test_case.oid, 'name': finding.test_case.requirement.readable_id}
