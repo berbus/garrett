bfb21c96aa89f7a4fed06e6d1131540afa475da0
Alberto <berbus8@gmail.com>
2022-08-21 14:16:34 +0200

    Bugfix: Parameter override in finding serializer

--- api/serializers/finding_serializer.py
+++ api/serializers/finding_serializer.py
@@ -4,11 +4,11 @@ import api.models as models
 
 
 class FindingSerializer(serializers.ModelSerializer):
-    test_case = serializers.SerializerMethodField(read_only=True)
+    test_case_data = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = models.Finding
         fields = '__all__'
 
-    def get_test_case(self, finding):
+    def get_test_case_data(self, finding):
         return {'oid': finding.test_case.oid, 'name': finding.test_case.requirement.readable_id}
